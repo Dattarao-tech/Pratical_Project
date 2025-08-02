c218bf6575723184fd35fc4e1e00510b05189569
Ganesh23-sketch <ganeshjaikar01@gmail.com>
2025-08-02 13:12:36 +0530

    gtest

---
+++ Dog.java
@@ -0,0 +1,23 @@
+public class Dog 
+{
+    String name;
+    String breed;
+    int age;
+    public Dog(String name, String breed, int age) 
+    {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+    public void displayDetails() 
+    {
+        System.out.println("Name: " + name);
+        System.out.println("Breed: " + breed);
+        System.out.println("Age: " + age + " years");
+    }
+    public static void main(String[] args) 
+    {
+        Dog myDog = new Dog("Buddy", "Golden Retriever", 3);
+        myDog.displayDetails();
+    }
+}
---
+++ Main.java
@@ -0,0 +1,24 @@
+class Airtel
+{   
+    static int n1=0,n2=1,n3=0;        
+     static void println(int count)
+     {        
+        if(count>0)
+        {        
+             n3 = n1 + n2;        
+             n1 = n2;        
+             n2 = n3;        
+             System.out.print(" "+n3);       
+             println(count-1);        
+         }        
+     }          
+}  
+public class Main
+{  
+ public static void main(String[] args)
+ {    
+    int count=15;        
+    System.out.println(F.n1+" "+F.n2);
+    Airtel.println(count-2);      
+ }    
+}    
